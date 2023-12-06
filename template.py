d344cb39b8d602b6adb9e081af1045d9696663a6
EdTheBass <68122094+EdTheBass@users.noreply.github.com>
2023-12-06 18:51:05 +0000

    Add files via upload

---
+++ template.py
@@ -0,0 +1,13 @@
+import re
+
+test_input = """
+""".splitlines()
+
+with open("dayN.txt", "r") as puzzle_input_file:
+    puzzle_input = puzzle_input_file.readlines()
+
+def _(inp):
+    # code
+    return
+
+print(_(test_input))
